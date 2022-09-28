d8f025d08d6b1af863d30e85aa1fd24c5212b3a6
kamrebaba <alamkamre58@gmail.com>
2022-09-28 22:01:37 +0530

    add somthing falnadhikna

--- src/Controllers/BookController.js
+++ src/Controllers/BookController.js
@@ -5,10 +5,43 @@ const valid=require("validator");
 const ReviewModel = require("../Models/ReviewModel");
 const aws=require('aws-sdk')
 
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRZNIRGT6N",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
+
+let uploadFile= async ( file) =>{
+   return new Promise( function(resolve, reject) {
+    // this function will upload file to aws and return the link
+    let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
+
+    var uploadParams= {
+        ACL: "public-read",
+        Bucket: "classroom-training-bucket",  //HERE
+        Key: "abc/" + file.originalname, //HERE 
+        Body: file.buffer
+    }
 
 
+    s3.upload( uploadParams, function (err, data ){
+        if(err) {
+            return reject({"error": err})
+        }
+        console.log(data)
+        console.log("file uploaded succesfully")
+        return resolve(data.Location)
+    })
+
+    // let data= await s3.upload( uploadParams)
+    // if( data) return data.Location
+    // else return "there is an error"
+
+   })
+}
 
 
+//updated somthing here
 let createBook= async function(req,res){
     
     try{
@@ -36,7 +69,19 @@ let createBook= async function(req,res){
         if(!validation(data.excerpt))  return res.status(400).send({status:false,msg:"excerpt is required"})
         if(!validation(data.category))  return res.status(400).send({status:false,msg:"category is required"})
         if(!validation(data.subcategory))  return res.status(400).send({status:false,msg:"subcategory is required"})
-
+        if(req.files[0]){
+            let files= req.files
+            if(files && files.length>0){
+                //upload to s3 and get the uploaded link
+                // res.send the link back to frontend/postman
+                var uploadedFileURL= await uploadFile( files[0] )
+                res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
+            }
+            else{
+                res.status(400).send({ msg: "No file found" })
+            }
+        }
+        data.bookCover= uploadedFileURL
         let savedUser= await BooksModel.create(data)
 
         return res.status(201).send({status:true,msg:"book created successfully",data:savedUser})
